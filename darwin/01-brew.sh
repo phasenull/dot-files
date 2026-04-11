f6485a79c08c957cd73c20af198cc521ed9496b6
phasenull <55253035+phasenull@users.noreply.github.com>
2026-04-12 00:05:10 +0300

    defaults

--- darwin/01-brew.sh
+++ darwin/01-brew.sh
@@ -1,5 +1,24 @@
-
+[[ "$OSTYPE" == darwin* ]] || exit 0
 source ~/.zshrc
+
+
+defaults write com.apple.dock autohide -bool true
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write com.apple.finder AppleShowAllFiles -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write NSGlobalDomain AppleLanguages -array "en-GB" "tr-TR"
+defaults write NSGlobalDomain "AppleInterfaceStyle" Dark;
+
+defaults write NSGlobalDomain "com.apple.keyboard.fnState" -int 1;
+defaults write NSGlobalDomain "com.apple.mouse.linear" -int 1;
+defaults write NSGlobalDomain "com.apple.mouse.scaling" -int 1;
+defaults write NSGlobalDomain "com.apple.sound.beep.flash" -int 0;
+defaults write NSGlobalDomain "com.apple.springing.delay" -float "0.5";
+defaults write NSGlobalDomain "com.apple.springing.enabled" -int 1;
+defaults write NSGlobalDomain "com.apple.swipescrolldirection" -int 1;
+defaults write NSGlobalDomain "com.apple.trackpad.forceClick" -int 1;
+
 if command -v brew &>/dev/null; then
   echo "[✔︎] brew already installed, updating..."
   brew update
